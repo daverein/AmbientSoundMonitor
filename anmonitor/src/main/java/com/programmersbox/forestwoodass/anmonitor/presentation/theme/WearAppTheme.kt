2fe733fedcb8a598869e7a8ae14a97a0549c6df9
David Rein <David.Rein.ext@unit4.com>
2023-03-07 18:55:52 -0500

    Remove colors from settings.

--- anmonitor/src/main/java/com/programmersbox/forestwoodass/anmonitor/presentation/theme/WearAppTheme.kt
+++ anmonitor/src/main/java/com/programmersbox/forestwoodass/anmonitor/presentation/theme/WearAppTheme.kt
@@ -1,7 +1,6 @@
 package com.programmersbox.forestwoodass.anmonitor.presentation.theme
 
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.platform.LocalContext
 import androidx.wear.compose.material.MaterialTheme
 
 @Composable
@@ -9,7 +8,7 @@ fun WearAppTheme(
     content: @Composable () -> Unit
 ) {
     MaterialTheme(
-        colors = getWearColorPalette(LocalContext.current),
+        colors = getWearColorPalette(),
         typography = Typography,
         // For shapes, we generally recommend using the default Material Wear shapes which are
         // optimized for round and non-round devices.
